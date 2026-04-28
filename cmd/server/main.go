7e6ca544760643c16a2c340c1c6957a2e69606df
Pham Trung Dung <dungphamtrung002@gmail.com>
2026-04-28 10:40:27 +0700

    create CORS

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"log"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 
 	"mangahub/internal/auth"
 	"mangahub/internal/grpc"
@@ -11,8 +15,6 @@ import (
 	"mangahub/internal/user"
 	ws "mangahub/internal/websocket"
 	"mangahub/pkg/database"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -34,6 +36,32 @@ func main() {
 	wsServer := ws.NewServer()
 	wsServer.RegisterRoutes(r)
 	go wsServer.Run()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"http://localhost:3000",
+			"http://localhost:5173",
+			"http://127.0.0.1:3000",
+			"http://127.0.0.1:5173",
+		},
+		AllowMethods: []string{
+			"GET",
+			"POST",
+			"PUT",
+			"DELETE",
+			"OPTIONS",
+		},
+		AllowHeaders: []string{
+			"Origin",
+			"Content-Type",
+			"Authorization",
+		},
+		ExposeHeaders: []string{
+			"Content-Length",
+		},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "pong"})
